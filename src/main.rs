782e11dd6cf18d0e8c1ebce2fe60037c4768b98f
Dan Munckton <dan@cultivatehq.com>
2017-09-15 10:42:55 +0100

    More tidying up

--- src/main.rs
+++ src/main.rs
@@ -7,51 +7,58 @@ use std::io::BufReader;
 use std::fs::File;
 use std::time::{Duration, Instant};
 
-use rand::Rng;
+use rand::{Rng, ThreadRng};
 use trivial_colours::{Colour, Reset};
 
 const WORD_LIST_FILE: &str = "/usr/share/dict/words";
 
 fn main() {
-    println!("{}{} v{}{}",
-             Colour::Blue,
-             env!("CARGO_PKG_NAME"),
-             env!("CARGO_PKG_VERSION"),
-             Reset);
+    show_banner();
 
     let start_time = Instant::now();
-    let timeout = Duration::from_secs(60);
-
+    let timeout = Duration::from_secs(10);
     let words = load_words(WORD_LIST_FILE);
-
     let mut rng = rand::thread_rng();
-    let mut input = String::new();
 
+    run(start_time, timeout, &mut rng, &words);
+}
+
+fn run(start_time: Instant, timeout: Duration, rng: &mut ThreadRng, words: &[String]) {
     loop {
         if start_time.elapsed() >= timeout {
             println!("Time's up!");
             break;
         }
 
+        let mut buffer = String::new();
         let word = rng.choose(&words).unwrap();
 
-        while word != input.trim() {
+        while word != buffer.trim() {
             let time_remaining = timeout.checked_sub(start_time.elapsed())
                                         .unwrap_or_default();
 
-            print!("{}{}s{} ",
-                   Colour::Magenta,
-                   time_remaining.as_secs(),
-                   Reset);
-            println!("{}{}{}", Colour::Cyan, word, Reset);
+            show_prompt(word, time_remaining);
 
-            print!("> ");
-            io::stdout().flush().expect("Error flushing stdout");
-
-            input.clear();
-            io::stdin().read_line(&mut input).expect("Error reading input");
+            buffer.clear();
+            io::stdin().read_line(&mut buffer).expect("Error reading input");
         }
     }
+}
+
+fn show_prompt(word: &str, time_remaining: Duration) {
+    print!("{}{}s{} ", Colour::Magenta, time_remaining.as_secs(), Reset);
+    print!("{}{}{}", Colour::Cyan, word, Reset);
+    println!();
+    print!("> ");
+    io::stdout().flush().expect("Error flushing stdout");
+}
+
+fn show_banner() {
+    println!("{}{} v{}{}",
+             Colour::Blue,
+             env!("CARGO_PKG_NAME"),
+             env!("CARGO_PKG_VERSION"),
+             Reset);
 }
 
 fn load_words(word_list_file: &str) -> Vec<String> {
